36b92d9a25375d8aa04123cfc4f7e0353ef7ed40
Vladyslav_T <tokhtaric_v_e@outlook.com>
2024-06-04 16:15:31 +0300

    Moved perl modules installing to Dockerfile and added wait-for util

--- opendkim/src/entrypoint.sh
+++ opendkim/src/entrypoint.sh
@@ -3,6 +3,10 @@
 # Replace vars
 envsubst < /etc/opendkim.conf.template > /etc/opendkim.conf
 
+# MySQL Server status 
+echo 'Waiting for the MySQL to start'
+./wait-for OPENDKIM_DB_SERVER:3306 -- echo "MySQL is up and running"
+
 /usr/bin/supervisord -c /etc/supervisor.conf
 sleep 5
 echo "The opendkim has been started"
